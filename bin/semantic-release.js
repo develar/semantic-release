9f79424590851412bf36aa36ee8624f83c52cdda
develar <develar@gmail.com>
2016-10-25 08:29:00 +0200

    get rid of npmconf

--- bin/semantic-release.js
+++ bin/semantic-release.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var path = require('path')
-var url = require('url')
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
 
-var _ = require('lodash')
-var log = require('npmlog')
-var nopt = require('nopt')
-var npmconf = require('npmconf')
-var normalizeData = require('normalize-package-data')
+const _ = require('lodash')
+const log = require('npmlog')
+const nopt = require('nopt')
+const rc = require('rc')
+const normalizeData = require('normalize-package-data')
 
 log.heading = 'semantic-release'
-var env = process.env
-var pkg = JSON.parse(fs.readFileSync('./package.json'))
-var originalPkg = _.cloneDeep(pkg)
+const env = process.env
+const pkg = JSON.parse(fs.readFileSync('./package.json'))
+const originalPkg = _.cloneDeep(pkg)
 normalizeData(pkg)
-var knownOptions = {
+const knownOptions = {
   branch: String,
   debug: Boolean,
   'github-token': String,
@@ -25,7 +25,7 @@ var knownOptions = {
   'verify-conditions': [path, String],
   'verify-release': [path, String]
 }
-var options = _.defaults(
+const options = _.defaults(
   _.mapKeys(nopt(knownOptions), function (value, key) {
     return _.camelCase(key)
   }),
@@ -40,122 +40,115 @@ var options = _.defaults(
     githubUrl: env.GH_URL
   }
 )
-var plugins = require('../src/lib/plugins')(options)
+const plugins = require('../src/lib/plugins')(options)
 
-npmconf.load({}, function (err, conf) {
-  if (err) {
-    log.error('init', 'Failed to load npm config.', err)
-    process.exit(1)
-  }
+const conf = rc('npm', {})
+const npm = {
+  auth: {
+    token: env.NPM_TOKEN
+  },
+  cafile: conf.get('cafile'),
+  loglevel: conf.get('loglevel'),
+  registry: require('../src/lib/get-registry')(pkg, conf),
+  tag: (pkg.publishConfig || {}).tag || conf.get('tag') || 'latest'
+}
 
-  var npm = {
-    auth: {
-      token: env.NPM_TOKEN
-    },
-    cafile: conf.get('cafile'),
-    loglevel: conf.get('loglevel'),
-    registry: require('../src/lib/get-registry')(pkg, conf),
-    tag: (pkg.publishConfig || {}).tag || conf.get('tag') || 'latest'
-  }
+// normalize trailing slash
+npm.registry = url.format(url.parse(npm.registry))
 
-  // normalize trailing slash
-  npm.registry = url.format(url.parse(npm.registry))
+log.level = npm.loglevel
 
-  log.level = npm.loglevel
+const config = {
+  env: env,
+  pkg: pkg,
+  options: options,
+  plugins: plugins,
+  npm: npm
+}
 
-  var config = {
-    env: env,
-    pkg: pkg,
-    options: options,
-    plugins: plugins,
-    npm: npm
-  }
+const hide = {}
+if (options.githubToken) hide.githubToken = '***'
 
-  var hide = {}
-  if (options.githubToken) hide.githubToken = '***'
+log.verbose('init', 'options:', Object.assign({}, options, hide))
+log.verbose('init', 'Verifying config.')
 
-  log.verbose('init', 'options:', Object.assign({}, options, hide))
-  log.verbose('init', 'Verifying config.')
+const errors = require('../src/lib/verify')(config)
+errors.forEach(function (err) {
+  log.error('init', err.message + ' ' + err.code)
+})
+if (errors.length) process.exit(1)
 
-  var errors = require('../src/lib/verify')(config)
-  errors.forEach(function (err) {
-    log.error('init', err.message + ' ' + err.code)
-  })
-  if (errors.length) process.exit(1)
+if (options.argv.remain[0] === 'pre') {
+  log.verbose('pre', 'Running pre-script.')
+  log.verbose('pre', 'Veriying conditions.')
 
-  if (options.argv.remain[0] === 'pre') {
-    log.verbose('pre', 'Running pre-script.')
-    log.verbose('pre', 'Veriying conditions.')
+  plugins.verifyConditions(config, function (err) {
+    if (err) {
+      log[options.debug ? 'warn' : 'error']('pre', err.message)
+      if (!options.debug) process.exit(1)
+    }
 
-    plugins.verifyConditions(config, function (err) {
-      if (err) {
-        log[options.debug ? 'warn' : 'error']('pre', err.message)
-        if (!options.debug) process.exit(1)
-      }
+    const nerfDart = require('nerf-dart')(npm.registry)
+    let wroteNpmRc = false
 
-      var nerfDart = require('nerf-dart')(npm.registry)
-      var wroteNpmRc = false
+    if (env.NPM_OLD_TOKEN && env.NPM_EMAIL) {
+      // Using the old auth token format is not considered part of the public API
+      // This might go away anytime (i.e. once we have a better testing strategy)
+      conf.set('_auth', '${NPM_OLD_TOKEN}', 'project')
+      conf.set('email', '${NPM_EMAIL}', 'project')
+      wroteNpmRc = true
+    } else if (env.NPM_TOKEN) {
+      conf.set(nerfDart + ':_authToken', '${NPM_TOKEN}', 'project')
+      wroteNpmRc = true
+    }
 
-      if (env.NPM_OLD_TOKEN && env.NPM_EMAIL) {
-        // Using the old auth token format is not considered part of the public API
-        // This might go away anytime (i.e. once we have a better testing strategy)
-        conf.set('_auth', '${NPM_OLD_TOKEN}', 'project')
-        conf.set('email', '${NPM_EMAIL}', 'project')
-        wroteNpmRc = true
-      } else if (env.NPM_TOKEN) {
-        conf.set(nerfDart + ':_authToken', '${NPM_TOKEN}', 'project')
-        wroteNpmRc = true
-      }
+    conf.save('project', function (error) {
+      if (error) return log.error('pre', 'Failed to save npm config.', error)
 
-      conf.save('project', function (error) {
-        if (error) return log.error('pre', 'Failed to save npm config.', error)
+      if (wroteNpmRc) log.verbose('pre', 'Wrote authToken to .npmrc.')
 
-        if (wroteNpmRc) log.verbose('pre', 'Wrote authToken to .npmrc.')
+      require('../src/pre')(config, function (error, release) {
+        if (error) {
+          log.error('pre', 'Failed to determine new version: ' + (error.stack || error))
+          process.exit(1)
+        }
 
-        require('../src/pre')(config, function (error, release) {
-          if (error) {
-            log.error('pre', 'Failed to determine new version: ' + (error.stack || error))
-            process.exit(1)
-          }
+        const message = 'Determined version ' + release.version + ' as "' + npm.tag + '".'
+        log.verbose('pre', message)
 
-          var message = 'Determined version ' + release.version + ' as "' + npm.tag + '".'
+        if (options.debug) {
+          log.error('pre', message + ' Not publishing in debug mode.', release)
+          process.exit(1)
+        }
 
-          log.verbose('pre', message)
+        try {
+          const shrinkwrap = JSON.parse(fs.readFileSync('./npm-shrinkwrap.json'))
+          shrinkwrap.version = release.version
+          fs.writeFileSync('./npm-shrinkwrap.json', JSON.stringify(shrinkwrap, null, 2))
+          log.verbose('pre', 'Wrote version ' + release.version + 'to npm-shrinkwrap.json.')
+        } catch (e) {
+          log.silly('pre', 'Couldn\'t find npm-shrinkwrap.json.')
+        }
 
-          if (options.debug) {
-            log.error('pre', message + ' Not publishing in debug mode.', release)
-            process.exit(1)
-          }
+        fs.writeFileSync('./package.json', JSON.stringify(Object.assign(originalPkg, {
+          version: release.version
+        }), null, 2))
 
-          try {
-            var shrinkwrap = JSON.parse(fs.readFileSync('./npm-shrinkwrap.json'))
-            shrinkwrap.version = release.version
-            fs.writeFileSync('./npm-shrinkwrap.json', JSON.stringify(shrinkwrap, null, 2))
-            log.verbose('pre', 'Wrote version ' + release.version + 'to npm-shrinkwrap.json.')
-          } catch (e) {
-            log.silly('pre', 'Couldn\'t find npm-shrinkwrap.json.')
-          }
-
-          fs.writeFileSync('./package.json', JSON.stringify(Object.assign(originalPkg, {
-            version: release.version
-          }), null, 2))
-
-          log.verbose('pre', 'Wrote version ' + release.version + ' to package.json.')
-        })
+        log.verbose('pre', 'Wrote version ' + release.version + ' to package.json.')
       })
     })
-  } else if (options.argv.remain[0] === 'post') {
-    log.verbose('post', 'Running post-script.')
+  })
+} else if (options.argv.remain[0] === 'post') {
+  log.verbose('post', 'Running post-script.')
 
-    require('../src/post')(config, function (err, published, release) {
-      if (err) {
-        log.error('post', 'Failed to publish release notes.', err)
-        process.exit(1)
-      }
+  require('../src/post')(config, function (err, published, release) {
+    if (err) {
+      log.error('post', 'Failed to publish release notes.', err)
+      process.exit(1)
+    }
 
-      log.verbose('post', (published ? 'Published' : 'Generated') + ' release notes.', release)
-    })
-  } else {
-    log.error('post', 'Command "' + options.argv.remain[0] + '" not recognized. Use either "pre" or "post"')
-  }
-})
+    log.verbose('post', (published ? 'Published' : 'Generated') + ' release notes.', release)
+  })
+} else {
+  log.error('post', 'Command "' + options.argv.remain[0] + '" not recognized. Use either "pre" or "post"')
+}
